e4e8be58400d5f67ec5f449b12ca26f544b225e4
Cristian Pirloaga <cristi@skyprivate.com>
2022-05-27 10:59:10 +0300

    saved

--- src/components/views/Display.js
+++ src/components/views/Display.js
@@ -9,12 +9,20 @@ import Invoice from './display/Invoice';
 
 const Display = (props) => {
 
+	const logout = function(){
+	
+		localStorage.removeItem('ef_secret');
+
+		window.location.reload()
+	}
+
 	return(
 		<div className="main">
 			
 			<div className="is-flex va-center">
 				<div className="col auto"> <UserName data={props.data}/> </div>
 				<div className="col auto"> <Daysoff data={props.data}/> </div>
+				<div className="col text-right"><button onClick={logout}>Logout</button></div>
 			</div>
 			
 			<Nav data={props.data}/>
--- src/components/views/display/Nav.js
+++ src/components/views/display/Nav.js
@@ -1,10 +1,5 @@
 export default function Nav(props) {
-	const logout = function(){
 	
-		localStorage.removeItem('ef_secret');
-
-		window.location.reload()
-	}
 	return (
 		
 		
@@ -15,7 +10,7 @@ export default function Nav(props) {
 
 			<div className="col text-center">
 				<h1 className="title blue">{props.data.user.month}</h1>
-				<button onClick={logout}>Logout</button>
+				
 			</div>
 			<div className="col text-center">
 				{props.data.user.nextmonth && <a href={props.data.currentMonth === 0 ? '/' : '?month='+(props.data.currentMonth+1)}><b>{props.data.user.nextmonth}&nbsp;<span className="icons">&gt;</span></b></a>}
