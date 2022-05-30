4b9e4802e34f628f280ad0d99c86a4b02a680689
Cristian Pirloaga <cristi@skyprivate.com>
2022-05-30 08:53:11 +0300

    saved

---
+++ src/components/views/display/Settings.js
@@ -0,0 +1,27 @@
+import {Component} from 'react';
+
+class Settings extends Component {
+
+	constructor(props) {
+
+		super(props);
+
+		this.state={}
+	
+	}
+
+
+	render(){
+
+		return (
+		
+			<>
+				AAAA
+			</>
+		)
+	
+	}
+}
+
+
+export default Settings;
