62ea6d4cd549f43ad3d3ec8b407315de32892a6e
Cristian Pirloaga <cristi@skyprivate.com>
2022-06-27 12:29:57 +0300

    save

--- src/components/display/Efficiency.js
+++ src/components/display/Efficiency.js
@@ -24,12 +24,28 @@ export default function Efficiency(props) {
 					<p>MonthDays: {props.data.monthInfo.monthDays}</p>
 					<p>WeekendDays: {props.data.monthInfo.weekendDays}</p>
 					<p>WorkDays: {props.data.monthInfo.workDays}</p>
+
+					<hr className="half-width" />
+					
 					<p className="green"><b>WorkedDays: {props.data.monthInfo.workedDays}</b></p>
 				</div>
 
 				<div className="col">
 					<p>WorkHours: {props.data.time.workHours}h</p>
-					<p className="green">WorkedHours: {props.data.time.workedHours}h</p>
+
+					<p className="is-flex va-center orange">Everhour:&nbsp;{props.data.time.everhour != null
+							? <span >{props.data.time.everhour > 0 
+									? props.data.time.everhour + 'h'
+									: <label><input onBlur={setEverhour} className="custom"/></label> 
+								}
+								</span>
+							: <ItemLoading data={{color: 'orange'}}/>
+						}
+					</p>
+					<p className="orange">Free days: {props.data.time.freedays}h</p>
+					<p className="orange">Days off: {props.data.time.daysoff}h</p>
+
+					
 
 					{ props.data.time.missingHours > 0  && 
 						<p className="red">Missing Hours: &nbsp;{props.data.time.missingHours != null
@@ -47,17 +63,9 @@ export default function Efficiency(props) {
 						</p>
 					}
 
-					<p className="is-flex va-center orange">Everhour:&nbsp;{props.data.time.everhour != null
-							? <span >{props.data.time.everhour > 0 
-									? props.data.time.everhour + 'h'
-									: <label><input onBlur={setEverhour} className="custom"/></label> 
-								}
-								</span>
-							: <ItemLoading data={{color: 'orange'}}/>
-						}
-					</p>
-					<p className="orange">Free days: {props.data.time.freedays}h</p>
-					<p className="orange">Days off: {props.data.time.daysoff}h</p>
+					<hr className="half-width" />
+
+					<p className="green">WorkedHours: {props.data.time.workedHours}h</p>
 
 				</div>
 
@@ -67,6 +75,9 @@ export default function Efficiency(props) {
 							: <ItemLoading />
 						}
 					</p>
+
+					<hr className="half-width" />
+					
 					<p className="green is-flex va-center"><b>Efficiency till today:&nbsp;{props.data.efficiency.current
 							? <span>{props.data.efficiency.current}%</span>
 							: <ItemLoading data={{color: 'green'}}/>
