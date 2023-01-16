a550e1dfb72a58bcbfea30ab1ea725cdfe2264c8
Cristian Pirloaga <cristi@skyprivate.com>
2023-01-16 09:42:47 +0200

    saved

--- config-sample.js
+++ config-sample.js
@@ -4,25 +4,6 @@ module.exports = {
       default: '/'
     }
   },
-  swagger : {
-    routePrefix: '/documentation',
-    exposeRoute: true,
-    swagger: {
-      info: {
-        title: 'Fastify API',
-        description: 'Building a blazing fast REST API with Node.js, MongoDB, Fastify and Swagger',
-        version: '1.0.0'
-      },
-      externalDocs: {
-        url: 'https://swagger.io',
-        description: 'Find more info here'
-      },
-      host: 'localhost',
-      schemes: ['http'],
-      consumes: ['application/json'],
-      produces: ['application/json']
-    }
-  },
   jwt : {
     secret : '1234567890'
   },
